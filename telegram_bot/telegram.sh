2163c3049bb7efd1a6aed82b3ed12a3efa289573
jeniatr <jeniatr3@gmail.com>
2021-03-03 00:26:59 +0200

    Improved telegram bot output

--- telegram_bot/telegram.sh
+++ telegram_bot/telegram.sh
@@ -1,16 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 token=$(head -1 t.conf)
 chat=$(sed -n 2p t.conf)
 
-echo "$token"
-echo "$chat"
+subject="${2}"
+message="${3}"
 
-subject="$1"
-echo "$subject"
+subject=$(echo "$subject" | sed 's/"/#/g')
+message=$(echo "$message" | sed 's/"/#/g')
 
-message="$2"
-echo "$message"
-curl -X POST -H 'Content-Type: application/json' -d "{\"chat_id\":\"${chat}\",\"text\":\"${subject}\n${message}\"}" https://api.telegram.org/bot"${token}"/sendMessage
+
+if [[ $subject =~ 'Problem' ]]; then
+        ICON="❗"
+elif [[ $subject =~ 'Resolved' ]]
+then
+        ICON="✔️"
+else
+        ICON="❓"
+fi
+
+message="$ICON $subject\n$message"
+curl -X POST -H 'Content-Type: application/json' -d "{\"chat_id\":\"${chat}\",\"text\":\"${message}\"}" https://api.telegram.org/bot"${token}"/sendMessage
+
 
 #    "Disaster": "🔥",
 #    "High": "🛑",
